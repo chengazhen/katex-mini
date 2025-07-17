0c852dee7528acb8b435137721db6601970a74b6
chengchongzhen <15939054361@163.com>
2025-07-17 15:28:24 +0800

    feat: :sparkles: Update dependencies and enhance configuration for Vite project, including addition of @types/node and removal of legacy vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,22 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// vite.config.ts
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [dts()],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "KatexMini",
-      // 将添加适当的扩展名后缀
-      fileName: "index",
+      entry: [resolve(__dirname, './src/index.ts')],
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      // 确保外部化处理那些
-      // 你不想打包进库的依赖
-      external: ["katex"],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖
-        // 提供一个全局变量
-        globals: {
-          katex: "katex",
-        },
-      },
-    },
+        exports: 'named',
+      }
+    }
   },
-});
+  plugins: [dts({
+    entryRoot: './src/index.ts',
+    exclude: './demo/**/*'
+  })]
+})
